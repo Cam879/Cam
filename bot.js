9cb773c992dfc6c7267203977834707552b83a65
Cam879 <65996153+Cam879@users.noreply.github.com>
2020-05-27 17:46:56 +1200

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,9 +15,8 @@ bot.on('ready', () => {
     bot.user.setStatus('available')
     bot.user.setPresence({
         game: {
-            name: 'with depression',
-            type: "STREAMING",
-            url: "https://www.twitch.tv/monstercat"
+            name: 'you sleep',
+            type: "WATCHING",
         }
     });
 });
