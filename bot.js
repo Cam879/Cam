141117fd0b26be8fcff711898a3a7b01babfc2e1
Cam879 <65996153+Cam879@users.noreply.github.com>
2020-05-27 20:43:52 +1200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,6 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('This bot is online!');
-    bot.user.setActivity('pacman world 2', { type: 'WATCHING'}).catch(console.error);
 });
 
 client.on('message', message => {
