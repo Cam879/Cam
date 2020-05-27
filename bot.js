194d0ba03a746897ec4c5ec0c5e3dcbe8d5c06ca
Cam879 <65996153+Cam879@users.noreply.github.com>
2020-05-27 18:23:16 +1200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+Client.on('ready', async () => {
+   await client.user.setPresence({ game: { name: 'name' }, status: 'idle' });
+});
 
 client.on('ready', () => {
     console.log('I am ready!');
