455e745c93f8ada019cb46f3e0a77f5f20a9e725
Cam879 <65996153+Cam879@users.noreply.github.com>
2020-05-27 20:15:40 +1200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
+    bot.user.setActivity('minecraft', { type: 'PLAYING'}).catch(console.error);
 });
 
 client.on('message', message => {
