c89b18b8f7b5f11451ecdb778f70252d7eab55ba
Cam879 <65996153+Cam879@users.noreply.github.com>
2020-05-27 19:57:03 +1200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,27 +5,10 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-bot.on('guildMemberAdd, member => {
-       
-       const channel = member.guild.channels.find(channel => channel.name === "👋┇welcome-and-leave");
-       if(!channel) return;
- 
-       channel.send(`Welcome to your server, ${member}, please read the rules in the rules channel!`)
-
-});
-
-client.on('message', message => {
-    if (message.content === 'HELLO') {
-    	message.reply('HELLO FRIEND');
-  	}
-    
- });
-
 client.on('message', message => {
     if (message.content === 'ping') {
     	message.reply('pong');
   	}
-    
 });
 
 // THIS  MUST  BE  THIS  WAY
