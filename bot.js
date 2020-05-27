a23ad8d6d4378662f8a2483f6506f553189cf2d8
Cam879 <65996153+Cam879@users.noreply.github.com>
2020-05-27 20:44:15 +1200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,6 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('This bot is online!');
-});
 
 client.on('message', message => {
     if (message.content === 'hi') {
